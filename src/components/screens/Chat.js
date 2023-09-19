197bcb668152b1e477ede685b54c3144f47be62c
Muhammad Zain <muhammadzainulabideen292@gmail.com>
2023-09-19 12:26:21 +0500

    commit

--- src/components/screens/Chat.js
+++ src/components/screens/Chat.js
@@ -1,29 +1,82 @@
-import {View, Text, TouchableOpacity} from 'react-native';
 import React, {useState, useCallback, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
-import '../../../FirebaseConfig';
 import firebase from 'firebase/compat';
+import {View, Text, TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useNavigation} from '@react-navigation/native';
-
-// Function to generate a unique chat ID based on participant user IDs
-function generateChatId(user1Id, user2Id) {
-  const sortedIds = [user1Id, user2Id].sort(); // Sort user IDs to ensure consistency
-  return sortedIds.join('_'); // Concatenate and hash the sorted user IDs
-}
 
 const Chat = ({route}) => {
   const {contactName, userId} = route.params;
   const [fullname, setFullName] = useState('');
   const [messages, setMessages] = useState([]);
-  const [selectedUserName, setSelectedUserName] = useState(''); // State to store the selected user's name
-
+  const [currentUser, setCurrentUser] = useState(null);
   const navigation = useNavigation();
 
   const user1Id = 1; // Replace with the actual user ID for the current user
 
-  // Generate the chat ID based on user IDs
-  const chatId = generateChatId(user1Id, userId);
+  // Fetch current user data from Firestore
+  useEffect(() => {
+    async function fetchCurrentUser() {
+      try {
+        const userDoc = await firebase
+          .firestore()
+          .collection('users')
+          .doc(user1Id.toString())
+          .get();
+        if (userDoc.exists) {
+          const userData = userDoc.data();
+          setCurrentUser(userData);
+          setFullName(userData.fullname);
+        }
+      } catch (error) {
+        console.error('Error fetching current user data:', error);
+      }
+    }
+
+    // Call the fetchCurrentUser function when the component mounts
+    fetchCurrentUser();
+  }, []);
+
+  // Function to determine the message style based on the sender
+  const renderMessage = message => {
+    if (message.user._id === user1Id) {
+      // Message sent by the current user (display in blue on the right)
+      return {
+        ...message,
+        user: {
+          ...message.user,
+          name: currentUser ? currentUser.fullname : '',
+        },
+        sent: true,
+        received: true,
+        position: 'right',
+        containerStyle: {
+          backgroundColor: '#007AFF',
+        },
+        textStyle: {
+          color: '#fff',
+        },
+      };
+    } else {
+      // Message sent by the other person (display in white on the left)
+      return {
+        ...message,
+        user: {
+          ...message.user,
+          name: contactName,
+        },
+        sent: true,
+        received: true,
+        position: 'left',
+        containerStyle: {
+          backgroundColor: '#fff',
+        },
+        textStyle: {
+          color: '#000',
+        },
+      };
+    }
+  };
 
   // Function to handle sending a message
   const onSend = useCallback(
@@ -42,7 +95,7 @@ const Chat = ({route}) => {
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             user: {
               _id: messageData.user._id,
-              name: fullname, // Replace with the actual user name
+              name: fullname,
             },
           });
 
@@ -57,88 +110,40 @@ const Chat = ({route}) => {
     [fullname],
   );
 
-  // Fetch messages from Firestore when the component mounts
-  useEffect(() => {
-    // Subscribe to messages in the Firestore subcollection
-    const unsubscribe = firebase
-      .firestore()
-      .collection('chats')
-      .doc(chatId)
-      .collection('messages')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot(snapshot => {
-        const messages = snapshot.docs
-          .filter(doc => doc.exists) // Filter out documents that don't exist
-          .map(doc => {
-            const data = doc.data();
-            const createdAt = data.createdAt ? data.createdAt.toDate() : null; // Check if createdAt exists
-            return {
-              _id: doc.id,
-              text: data.text,
-              createdAt,
-              user: data.user,
-            };
-          });
-
-        setMessages(messages);
-      });
-
-    return () => unsubscribe(); // Unsubscribe when the component unmounts
-  }, []);
-
-  // Fetch the sender's name from the "users" collection
-  useEffect(() => {
-    async function fetchSenderName() {
-      try {
-        const userDoc = await firebase
-          .firestore()
-          .collection('users')
-          .doc(userId)
-          .get();
-        if (userDoc.exists) {
-          const senderName = userDoc.data().fullname;
-          // Update the selected user's name in state
-          setSelectedUserName(senderName);
-        }
-      } catch (error) {
-        console.error('Error fetching sender name:', error);
-      }
-    }
-
-    // Call the fetchSenderName function when the component mounts and when the selected user changes
-    fetchSenderName();
-  }, [userId]);
+  const chatId = generateChatId(user1Id, userId);
 
   return (
-    <View style={{flex: 1}}>
-      <View
-        style={{padding: 16, borderBottomWidth: 1, borderBottomColor: '#ccc'}}>
+    <View className="flex-1">
+      <View className='p-4 bg-white shadow-md'>
         <View className="flex-row">
-          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <TouchableOpacity onPress={() => navigation.navigate('Home')} className='translate-y-1'>
             <MaterialCommunityIcons
               name="arrow-left-thin"
-              size={28}
-              color="#f57c00"
+              size={30}
+              color={'#f57c00'}
             />
           </TouchableOpacity>
-          <Text className="text-black font-bold text-[20px] translate-x-3">
-            {contactName}
-          </Text>
+          <Text className='text-[24px] px-2'>{contactName}</Text>
         </View>
       </View>
       <GiftedChat
-        messages={messages}
-        onSend={newMessages => onSend(newMessages)}
+        messages={messages.map(renderMessage)}
+        onSend={onSend}
         user={{
-          _id: user1Id, // Unique ID for the user (you can use any value)
-          name: fullname, // Replace with the actual user name
+          _id: user1Id,
+          name: fullname,
         }}
-        renderUsernameOnMessage={true} // Display sender's name in message bubbles
+        renderUsernameOnMessage={true}
         alwaysShowSend
         placeholder="Type your message..."
       />
     </View>
   );
 };
+
+function generateChatId(user1Id, user2Id) {
+  const sortedIds = [user1Id, user2Id].sort(); // Sort user IDs to ensure consistency
+  return sortedIds.join('_'); // Concatenate and hash the sorted user IDs
+}
 
 export default Chat;
